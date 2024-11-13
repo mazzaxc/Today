d2288ff1c610038f220ce67279c319364bf00d94
Lorenzo Mazzarotto <mazzaxc@icloud.com>
2024-11-13 17:14:13 -0300

    Make the model identifiable

--- Today/ListViewController./ReminderListViewController+DataSource.swift
+++ Today/ListViewController./ReminderListViewController+DataSource.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 extension ReminderListViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
-        let reminder = Reminder.sampleData[indexPath.item]
+    func cellRegistrationHandler(
+        cell:
+        UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID
+    ) {
+        let reminder = reminders[indexPath.item]
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
@@ -26,7 +29,7 @@ extension ReminderListViewController {
             .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)
         ]
         
-        var backgroundConfiguration = UIBackgroundConfiguration.listCell()
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
     }
