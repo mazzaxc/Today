a8d199c25dab8248626f417e6c986fa481def661
Lorenzo Mazzarotto <mazzaxc@icloud.com>
2024-11-14 19:37:41 -0300

    Create a reminder view

---
+++ Today/ReminderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Lorenzo Mazzarotto on 14/11/24.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+}
