d09136b01592b6f0ed4522bd8c5890dfbaa4bd15
Lorenzo Mazzarotto <mazzaxc@icloud.com>
2024-12-13 23:30:06 -0300

    Read reminders individually

--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -11,6 +11,7 @@ enum TodayError: LocalizedError {
     
     case accessDenied
     case accessRestricted
+    case failedReadingCalendarItem
     case failedReadingReminders
     case reminderHasNoDueDate
     case unknown
@@ -25,6 +26,9 @@ enum TodayError: LocalizedError {
             return NSLocalizedString(
                 "This device doesn't allow access to reminders.",
                 comment: "access restricted error description")
+        case .failedReadingCalendarItem:
+            return NSLocalizedString(
+                "Failed to read a calendar item.", comment: "failed reading calendar item error description")
         case .failedReadingReminders:
             return NSLocalizedString(
                 "Failed to read reminders.", comment: "failed reading reminders error description")
